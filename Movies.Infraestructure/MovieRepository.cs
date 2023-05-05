2022c8cf2785bfc547a51dafc9dd421a3ef4f39b
Budula <williammalheiros_2@hotmail.com>
2023-05-05 13:52:14 -0300

    Added GetMoviesByDirectorName in infraestructure Layer

--- Movies.Infraestructure/MovieRepository.cs
+++ Movies.Infraestructure/MovieRepository.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+using Movies.Core.Repositories;
+using Movies.Infraestructure.Data;
+using Movies.Infraestructure.Repositories.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,17 @@ using System.Threading.Tasks;
 
 namespace Movies.Infraestructure
 {
-    internal class MovieRepository
+    public class MovieRepository : Repository<Movie>, IMovieRepository
     {
+        public MovieRepository(MovieContext movieContext):base(movieContext)
+        {
+            
+        }
+        public async Task<IEnumerable<Movie>> GetMoviesByDirectorName(string directorName)
+        {
+            return await _movieContext.Movies
+                .Where(m => m.DirectorName.Equals(directorName))
+                .ToListAsync();
+        }
     }
 }
